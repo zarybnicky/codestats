2adc9b7bff1752f53509c00d8127dc16b9adf72f
Jakub Zárybnický <jakub@zarybnicky.com>
2023-11-22 15:38:21 +0100

    Scaffold

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/release-23.05;
+  inputs.devenv.url = github:cachix/devenv;
+  inputs.graphile-migrate-flake.url = github:zarybnicky/graphile-migrate-flake;
+
+  outputs = { self, nixpkgs, devenv, graphile-migrate-flake, ... } @ inputs: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [
+        graphile-migrate-flake.overlays.default
+      ];
+    };
+  in {
+    devenv-up = self.devShells.x86_64-linux.default.config.procfileScript;
+    devShells.x86_64-linux.default = devenv.lib.mkShell {
+      inherit inputs pkgs;
+      modules = [
+        ({ pkgs, ... }: {
+          packages = [
+            pkgs.graphile-migrate
+            pkgs.postgresql_15
+            pkgs.sqlint
+            pkgs.pgformatter
+          ];
+
+          services.postgres = {
+            enable = true;
+            package = pkgs.postgresql_15;
+            initialDatabases = [
+              { name = "mergestat"; }
+              { name = "mergestat-shadow"; }
+            ];
+            initialScript = ''
+              CREATE USER postgres SUPERUSER;
+            '';
+          };
+        })
+      ];
+    };
+  };
+}
